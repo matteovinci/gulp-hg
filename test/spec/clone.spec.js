3ede1392701ed875ee15408f9669884a04853ce0
matteovinci <matteovinci88@gmail.com>
2015-12-21 11:52:03 -0800

    clone.spec changes

--- test/spec/clone.spec.js
+++ test/spec/clone.spec.js
@@ -11,23 +11,17 @@ require('shelljs/global');
 module.exports = function(hg) {
 
     var cloneDestinationPath = testsuite.repoTestFolders[1];
-    var cwd = process.cwd();
 
     beforeEach(function(done) {
         hg.clone(testsuite.remoteRepository, cloneDestinationPath, function(err) {
-            cd(cloneDestinationPath);
             should(err).not.exists;
             done();
         });
-
     });
 
     it('should have cloned project into a specific directory', function(done) {
+        cd(cloneDestinationPath);
         hg.utils.isHg().should.be.equal(true);
         done();
-    });
-
-    afterEach(function() {
-        cd(cwd);
     });
 };
